cc6fc60205e5b122c7488ea3ada7c9b6679158e9
VikasHBishnoi <vikasbishnoi992858@gmail.com>
2023-06-29 18:55:28 +0530

    Dynamic notes added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,17 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import allNotes from "../Notes";
+// function makeNote(note) {
+//     return <Note key={note.key} title={note.title} content={note.content} />;
+// }
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {allNotes.map(note => {
+                return <Note key={note.key} title={note.title} content={note.content} />
+            })}
             <Footer />
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-function Node(){
-    return <div className="note"><h1>Heading</h1>Content toa padhai me hi hai<p></p></div>;
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
 }
-export default Node;
+export default Note;
